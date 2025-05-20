882d26d3e13bfec9a2a151172ec6bc9b81136a72
Abongile Arthur Mavela <74903148+Abongile184@users.noreply.github.com>
2025-05-20 22:17:05 +0200

    Update Statistics.js

--- src/stats/Statistics.js
+++ src/stats/Statistics.js
@@ -1,6 +1,7 @@
 import Chart from 'chart.js/auto';
 import Storage from '../Storage/Storage';
-import { isFuture, parseISO } from 'date-fns'; 
+import { isFuture, isBefore, startOfDay, parseISO } from 'date-fns';
+
 import getChartConfig from './chartConfig';
 
 class Statistics {
@@ -16,14 +17,23 @@ class Statistics {
     }
 
     generateChart() {
-        const tasks = Storage.getTasks();
-        
-        const completedTasks = tasks.filter(task => task.complete).length;
-        const upcomingTasks = tasks.filter(task => isFuture(parseISO(task.dueDate)) && !task.complete).length;
-        const pendingTasks = tasks.length - (completedTasks + upcomingTasks);
+    const tasks = Storage.getTasks();
+    const today = startOfDay(new Date());
 
-        const ctx = document.getElementById('taskChart').getContext('2d');
-        new Chart(ctx, getChartConfig(completedTasks, upcomingTasks, pendingTasks));
+    const completedTasks = tasks.filter(task => task.complete).length;
+    const upcomingTasks = tasks.filter(task => {
+        const taskDate = parseISO(task.dueDate);
+        return isFuture(taskDate) && !task.complete;
+    }).length;
+    const overdueTasks = tasks.filter(task => {
+        const taskDate = startOfDay(parseISO(task.dueDate));
+        return !task.complete && isBefore(taskDate, today);
+    }).length;
+
+    const pendingTasks = tasks.length - (completedTasks + upcomingTasks + overdueTasks);
+
+    const ctx = document.getElementById('taskChart').getContext('2d');
+    new Chart(ctx, getChartConfig(completedTasks, upcomingTasks, pendingTasks, overdueTasks));
     }
 }
 
